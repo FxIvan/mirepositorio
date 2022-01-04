98d240adc7b297416ec22d32fb04a3f6dd2776d6
FxIvan <almendraivan210814@gmail.com>
2022-01-04 11:22:18 -0300

    s

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path='/mirepositorio' element={<Principal/>}/>
+          <Route path='/' element={<Principal/>}/>
         </Routes>
       </BrowserRouter>
 
